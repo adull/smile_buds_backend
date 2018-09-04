a2b56d38da2dc57ffc5e3e8cdd248b630da2bd0c
adull <adlaiabdelrazaq@gmail.com>
2018-09-04 15:19:35 -0500

    updated get image post

--- app/routes/getImageFromPost.js
+++ app/routes/getImageFromPost.js
@@ -3,6 +3,10 @@ module.exports = function(router) {
   .get(function(req, res) {
     let hash = req.params.hash;
     let path = '/uploads/' + hash + '.png';
+    let fuckYou = '/profile-pictures/a.png';
+    if(hash === undefined || hash === '') {
+      res.sendFile(fuckYou, {root:__dirname});
+    }
     res.sendFile(path, {root:__dirname});
   })
 }
--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var database = require('./app/config/initializers/database.js')
 var async = require('async');
 require('dotenv').config();
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus().length;
 
 
-if (cluster.isMaster) {
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-}
-else {
+// if (cluster.isMaster) {
+//   for (var i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+//
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//   });
+// }
+// else {
   // console.log("started")
-  // module.exports = start;
+  module.exports = start;
   async.series([
     // function initializeDBConnection(callback) {
     //   database(callback);
@@ -33,4 +33,4 @@ else {
       }
     })
 
-}
+// }
