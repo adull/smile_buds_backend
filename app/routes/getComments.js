f61640381782ef4dd685c7ad80f8e9daf6934084
adull <adlaiabdelrazaq@gmail.com>
2018-09-23 13:45:57 -0500

    trying to fix get comments further

--- app/routes/getComments.js
+++ app/routes/getComments.js
@@ -3,92 +3,32 @@ var db = require('../config/initializers/database');
 module.exports = function(router) {
   router.route('/:hash')
   .get(function(req, res, next) {
-    let hash = req.params.hash;
-    let userid = req.session.userid;
-    let userIdentifier = req.session.identifier;
-    let isAdmin = false;
+    try {
+      let hash = req.params.hash;
+      let userid = req.session.userid;
+      let userIdentifier = req.session.identifier;
+      let isAdmin = false;
 
-    db.getComments(hash, function(err, getCommentsResults) {
-      if(err) {
-        res.status(500).send("Server error");
-        return;
-      }
-      else {
-        db.getPost(hash, function(err, getPostResult) {
-          if(err) {
-            res.status(500).send("Server error");
-            return;
-          }
-          if(getPostResult[0].poster_id === userid) {
-            for(var i = 0; i < getCommentsResults.length; i ++) {
-              getCommentsResults[i].deletePermission = true;
-            }
-            if(getCommentsResults) {
-              var commentGrinsArr = [];
-              var individualCommentGrinsArr = [];
-              db.getCommentGrins(getCommentsResults, function(err, getCommentGrinsResults) {
-                if(err) {
-                  res.status(500).send("Server error");
-                  return;
-                }
-                else {
-                  for(var i = 0; i < getCommentGrinsResults.length; i++) {
-                    if(userIdentifier) {
-                      if(getCommentGrinsResults[i].grinner_identifier === userIdentifier) {
-                        getCommentGrinsResults[i].didIGrin = true;
-                      }
-                      else {
-                        getCommentGrinsResults[i].didIGrin = false;
-                      }
-                    }
-                    else {
-                      getCommentGrinsResults[i].didIGrin = false;
-                    }
-                  }
-                  if(getCommentsResults && getCommentGrinsResults) {
-                    res.json({commentResults: getCommentsResults, commentGrins: getCommentGrinsResults});
-                    return;
-                  }
-                  else {
-                    res.end();
-                  }
-                }
-              })
-            }
-            else {
-              res.end();
+      db.getComments(hash, function(err, getCommentsResults) {
+        if(err) {
+          res.status(500).send("Server error");
+          return;
+        }
+        else {
+          db.getPost(hash, function(err, getPostResult) {
+            if(err) {
+              res.status(500).send("Server error");
               return;
             }
-          }
-          else if(userid) {
-            db.getUser('userid', userid, function(err, getUserResult) {
-              if(err) {
-                console.log(65)
-                res.status(500).send("Server error");
-                return;
-              }
-              if(getUserResult[0]) {
-                if(getUserResult[0].type === "admin") {
-                  isAdmin = true;
-                }
-              }
+            if(getPostResult[0].poster_id === userid) {
               for(var i = 0; i < getCommentsResults.length; i ++) {
-                if(isAdmin) {
-                  getCommentsResults[i].deletePermission = true;
-                }
-                else if(getCommentsResults[i].commenter_id === userid) {
-                  getCommentsResults[i].deletePermission = true
-                }
-                else {
-                  getCommentsResults[i].deletePermission = false;
-                }
+                getCommentsResults[i].deletePermission = true;
               }
               if(getCommentsResults) {
                 var commentGrinsArr = [];
                 var individualCommentGrinsArr = [];
                 db.getCommentGrins(getCommentsResults, function(err, getCommentGrinsResults) {
                   if(err) {
-                    console.log(90)
                     res.status(500).send("Server error");
                     return;
                   }
@@ -120,50 +60,118 @@ module.exports = function(router) {
                 res.end();
                 return;
               }
-            })
-          }
-          else {
-            for(var i = 0; i < getCommentsResults.length; i ++) {
-              getCommentsResults[i].deletePermission = false;
             }
-            if(getCommentsResults) {
-              var individualCommentGrinsArr = [];
-              db.getCommentGrins(getCommentsResults, function(err, getCommentGrinsResults) {
+            else if(userid) {
+              db.getUser('userid', userid, function(err, getUserResult) {
                 if(err) {
+                  console.log(65)
                   res.status(500).send("Server error");
                   return;
                 }
+                if(getUserResult[0]) {
+                  if(getUserResult[0].type === "admin") {
+                    isAdmin = true;
+                  }
+                }
+                for(var i = 0; i < getCommentsResults.length; i ++) {
+                  if(isAdmin) {
+                    getCommentsResults[i].deletePermission = true;
+                  }
+                  else if(getCommentsResults[i].commenter_id === userid) {
+                    getCommentsResults[i].deletePermission = true
+                  }
+                  else {
+                    getCommentsResults[i].deletePermission = false;
+                  }
+                }
+                if(getCommentsResults) {
+                  var commentGrinsArr = [];
+                  var individualCommentGrinsArr = [];
+                  db.getCommentGrins(getCommentsResults, function(err, getCommentGrinsResults) {
+                    if(err) {
+                      // console.log(90)
+                      res.status(500).send("Server error");
+                      return;
+                    }
+                    else {
+                      for(var i = 0; i < getCommentGrinsResults.length; i++) {
+                        if(userIdentifier) {
+                          if(getCommentGrinsResults[i].grinner_identifier === userIdentifier) {
+                            getCommentGrinsResults[i].didIGrin = true;
+                          }
+                          else {
+                            getCommentGrinsResults[i].didIGrin = false;
+                          }
+                        }
+                        else {
+                          getCommentGrinsResults[i].didIGrin = false;
+                        }
+                      }
+                      if(getCommentsResults && getCommentGrinsResults) {
+                        res.json({commentResults: getCommentsResults, commentGrins: getCommentGrinsResults});
+                        return;
+                      }
+                      else {
+                        res.end();
+                      }
+                    }
+                  })
+                }
                 else {
-                  for(var i = 0; i < getCommentGrinsResults.length; i++) {
-                    if(userIdentifier) {
-                      if(getCommentGrinsResults[i].grinner_identifier === userIdentifier) {
-                        getCommentGrinsResults[i].didIGrin = true;
+                  res.end();
+                  return;
+                }
+              })
+            }
+            else {
+              for(var i = 0; i < getCommentsResults.length; i ++) {
+                getCommentsResults[i].deletePermission = false;
+              }
+              if(getCommentsResults) {
+                var individualCommentGrinsArr = [];
+                db.getCommentGrins(getCommentsResults, function(err, getCommentGrinsResults) {
+                  if(err) {
+                    res.status(500).send("Server error");
+                    return;
+                  }
+                  else {
+                    for(var i = 0; i < getCommentGrinsResults.length; i++) {
+                      if(userIdentifier) {
+                        if(getCommentGrinsResults[i].grinner_identifier === userIdentifier) {
+                          getCommentGrinsResults[i].didIGrin = true;
+                        }
+                        else {
+                          getCommentGrinsResults[i].didIGrin = false;
+                        }
                       }
                       else {
                         getCommentGrinsResults[i].didIGrin = false;
                       }
                     }
+                    if(getCommentsResults && getCommentGrinsResults) {
+                      res.json({commentResults: getCommentsResults, commentGrins: getCommentGrinsResults});
+                      return;
+                    }
                     else {
-                      getCommentGrinsResults[i].didIGrin = false;
+                      res.end();
                     }
                   }
-                  if(getCommentsResults && getCommentGrinsResults) {
-                    res.json({commentResults: getCommentsResults, commentGrins: getCommentGrinsResults});
-                    return;
-                  }
-                  else {
-                    res.end();
-                  }
-                }
-              })
+                })
+              }
+              else {
+                res.end();
+                return;
+              }
             }
-            else {
-              res.end();
-              return;
-            }
-          }
-        })
-      }
-    })
+          })
+        }
+      })
+    }
+    catch(error) {
+      console.log(171)
+      console.log(error);
+      res.end();
+    }
+
   })
 }
