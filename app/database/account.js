68be03fb05c68b3ba145692b0526bcb1279c24e8
Payton Bice <hellopayton@gmail.com>
2018-09-23 16:36:32 -0500

    Makes create() asynchronous

--- app/database/account.js
+++ app/database/account.js
@@ -1,20 +1,5 @@
 const mysql = require('mysql');
 
-/**
- * Contains functionality pertaining to user accounts and settings.
- *
- * Example: In routes/signup.js, the create() method will be called in lieu of db.signup():
- * 
- * ```
- * const User = require('./database/account.js')
- * // ...
- * User.create({foo: 'bar'})
- *  .then(results => {})
- *  .catch(error => {})
- * ```
- * 
- * (This comment may be removed after this change is approved or rejected)
- */
 module.exports = {
     /**
      * Adds a user to the database
@@ -22,7 +7,7 @@ module.exports = {
      * @param {Object} data An object containing the user's data
      * @returns {Promise} Promise
      */
-    create: (data) => {
+    create: async (data) => {
         return new Promise((resolve, reject) => {
             pool.query('INSERT INTO `user` SET ?', data, (error, results) => {
                 if (error) {
