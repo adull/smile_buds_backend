140cb49939fbbb6d11eb92c7792f434f12e2a1ab
Payton Bice <hellopayton@gmail.com>
2018-09-24 11:45:05 -0500

    Adds updateHobby() with escaped values to prevent SQL injection

--- app/database/account.js
+++ app/database/account.js
@@ -9,12 +9,31 @@ module.exports = {
      */
     create: async (data) => {
         return new Promise((resolve, reject) => {
-            pool.query('INSERT INTO `user` SET ?', data, (error, results) => {
+            pool.query('INSERT INTO `user` SET ?', data, (error, result) => {
                 if (error) {
                     reject(error);
                 }
 
-                resolve(results);
+                resolve(result);
+            });
+        });
+    },
+
+    /**
+     * Updates a user's hobby
+     * 
+     * @param {String} hobby
+     * @param {String} identifier
+     * @returns {Promise}
+     */
+    updateHobby: async (hobby, identifier) => {
+        return new Promise((resolve, reject) => {
+            pool.query('UPDATE user SET hobby = ? WHERE identifier ?', [hobby, identifier], (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+
+                resolve(result);
             });
         });
     }
